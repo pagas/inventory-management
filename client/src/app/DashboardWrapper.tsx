b8c7e6c0aa83808b77d2785e2c98a5ad5b67a25b
Paulius G <paulius@2simple.com>
2024-08-02 14:37:24 +0100

    Add Navbar component

--- client/src/app/DashboardWrapper.tsx
+++ client/src/app/DashboardWrapper.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Navbar from '@/app/(components)/Navbar';
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div>{children}</div>
+        <div className={`light flex bg-gray-50 text-gray-900 w-fll min-h-screen`}>
+            Sidebar
+            <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}>
+                <Navbar />
+                {children}
+            </main>
+
+        </div>
     )
 }
 
